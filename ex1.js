1536f389c71ec05b5120ca85f5257076344730ee
FelipeGabriel05 <felpsgb2321@gmail.com>
2023-05-24 22:24:05 -0300

    Media

---
+++ ex1.js
@@ -0,0 +1,10 @@
+var n1 = 2
+var n2 = 5
+var n3 = 4
+if (n1 > n2 && n1 > n3) {
+    console.log(`O número ${n1} é maior que ${n2} e ${n3} `)
+} else if (n2 > n1 && n2 > n3) {
+    console.log(`O número ${n2} é maior que ${n1} e ${n3}`)
+} else {
+    console.log(`O número ${n3} é maior que ${n1} e ${n2}`)
+}
